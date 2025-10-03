f73b34b712bf93d6284a3f9b6de08847d47a7f2e
AlexisH99 <alexis.huart.99@gmail.com>
2025-10-03 19:12:32 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "config.hpp"
+#include <iostream>
+
+int main() {
+
+    std::cout << "This is a test\n";
+    std::cout << "Height: " << HEIGHT << "\n";
+    std::cout << "Width: " << WIDTH << "\n";
+
+    return 0;
+}
